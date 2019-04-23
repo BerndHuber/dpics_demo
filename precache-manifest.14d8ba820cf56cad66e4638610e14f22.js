8407a67cd77ed0f549435fa9381457f3c88290c8
bhb123 <bhb@seas.harvard.edu>
2019-04-23 13:08:42 -0400

    Add files via upload

---
+++ precache-manifest.14d8ba820cf56cad66e4638610e14f22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa28006f3f836fc867ecc23d18397a25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f80bf3c98278f60b1d18",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "36c6d2ff2f153374c56b",
+    "url": "/static/js/2.4834304d.chunk.js"
+  },
+  {
+    "revision": "f80bf3c98278f60b1d18",
+    "url": "/static/js/main.6d30322b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
