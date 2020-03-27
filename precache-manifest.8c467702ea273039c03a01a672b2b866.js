a4720f9b87082aa876c3fb5225075d92f9575646
bhb123 <bhb@seas.harvard.edu>
2020-03-27 11:34:24 -0400

    Add files via upload

---
+++ precache-manifest.8c467702ea273039c03a01a672b2b866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240b78f542e33bac4a00acb4b18133fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "612e7128c97b3677649a",
+    "url": "/static/css/main.9e55f283.chunk.css"
+  },
+  {
+    "revision": "36c6d2ff2f153374c56b",
+    "url": "/static/js/2.4834304d.chunk.js"
+  },
+  {
+    "revision": "612e7128c97b3677649a",
+    "url": "/static/js/main.03fa4bcd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c571efba763dfe6e81d52ce5721a7a2.js"
+  "/precache-manifest.8c467702ea273039c03a01a672b2b866.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
